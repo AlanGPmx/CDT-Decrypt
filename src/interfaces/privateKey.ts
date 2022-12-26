b6f53aaea32222c7f548fe094911529e91a90b59
Alan Garduño Pineda <alansgp@outlook.com>
2022-12-26 11:52:57 -0600

    Interfaces creadas

---
+++ src/interfaces/privateKey.ts
@@ -0,0 +1,4 @@
+export interface PrivateKey {
+	llavePrivada: string;
+	texto: any;
+}
---
+++ src/interfaces/texto.ts
@@ -0,0 +1,5 @@
+export interface Texto {
+	idAcceso: string;
+	completo: string;
+	paraDesencriptar: string;
+}
