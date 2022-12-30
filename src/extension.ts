4ba58d6716f3dd56ac2dbe163e956d3342d1aa24
Alan Garduño Pineda <alansgp@outlook.com>
2022-12-30 10:43:48 -0600

    fix: v0.0.5 -> Uso del servicio de David

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,8 @@ import {Texto} from './interfaces/texto';
 import {desencriptarObjeto} from './utils/Desencriptar';
 import {getPrivateKey} from './keys/getPrivateKey';
 import * as path from 'path';
+import {cleanText} from './utils/cleanText';
+import {testServiceDavis, useServiceDavis} from './utils/ServicioDavid';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -26,15 +28,14 @@ export function activate(context: vscode.ExtensionContext) {
 			// Obtener el texto del editor activo
 			const texto: Texto = {
 				idAcceso: '',
-				completo: editor.document.getText().replace(/(\r\n|\n|\r)/gm, ''),
-				paraDesencriptar: editor.document
-					.getText(editor.selection)
-					.replace(/(\r\n|\n|\r)/gm, ''),
+				completo: cleanText(editor.document.getText().trim()).replace(
+					/(\r\n|\n|\r)/gm,
+					''
+				),
+				paraDesencriptar: cleanText(
+					editor.document.getText(editor.selection)
+				).replace(/(\r\n|\n|\r)/gm, ''),
 			};
-
-			let pathToSave =
-				editor.document.uri.path.split('/').slice(0, -1).join('/') + '/';
-			pathToSave = '' + pathToSave.substring(1);
 
 			// Pedir el idAcceso mediante un input box
 			vscode.window
@@ -51,7 +52,13 @@ export function activate(context: vscode.ExtensionContext) {
 						},
 						async (progress) => {
 							progress.report({message: `Desencriptando ando...`});
-							await logica(idAcceso, texto, pathToSave);
+							try {
+								await logica(idAcceso, texto);
+							} catch (err) {
+								const msg: string = (err as any).message;
+								vscode.window.showErrorMessage(`Error: ${msg}`);
+								console.log(err);
+							}
 						}
 					);
 				});
@@ -60,9 +67,10 @@ export function activate(context: vscode.ExtensionContext) {
 
 	const removeFilePart = (dirname: any) => path.parse(dirname).dir;
 
-	const logica = async (idAcceso: string, texto: Texto, pathToSave: any) => {
+	const logica = async (idAcceso: string, texto: Texto) => {
+		let pathToSave: any;
 		if (idAcceso === '' || idAcceso === undefined) {
-			return vscode.window.showInformationMessage(
+			return vscode.window.showErrorMessage(
 				`El el idAcceso no puede estar vacio`
 			);
 		}
@@ -72,27 +80,16 @@ export function activate(context: vscode.ExtensionContext) {
 				'No se ha abierto ninguna carpeta o espacio de trabajo, el archivo se guardará en la misma carpeta donde se encuentra el LOG',
 				'Ok'
 			);
-
 			pathToSave = vscode.window.activeTextEditor?.document.uri;
 		} else {
 			pathToSave = vscode.workspace.workspaceFolders[0].uri;
 		}
 
-		const textoCompletoUnaLinea = texto.completo.replace(/(\r\n|\n|\r)/gm, '');
+		const textoCompletoUnaLinea = texto.completo
+			.replace(/(\r\n|\n|\r)/gm, '')
+			.toString();
 		let response = getPrivateKey(textoCompletoUnaLinea, texto, 1); // Response
 		let request = getPrivateKey(textoCompletoUnaLinea, texto, 2); // Request
-
-		/**
-		 * Desencriptado
-		 */
-		let requestDecrypted = await desencriptarObjeto(
-			JSON.parse(request.texto),
-			request.llavePrivada
-		);
-		let responseDecrypted = await desencriptarObjeto(
-			JSON.parse(response.texto),
-			response.llavePrivada
-		);
 
 		let fechaActual: Date | string = new Date();
 		const opciones: any = {
@@ -106,7 +103,48 @@ export function activate(context: vscode.ExtensionContext) {
 		};
 		fechaActual = fechaActual.toLocaleDateString('es-mx', opciones);
 
-		let textoFinal: string = `\n
+		/**
+		 * Desencriptado
+		 */
+		const statusDavisService: any = await testServiceDavis();
+		let requestDecrypted: any;
+		let responseDecrypted: any;
+
+		if (statusDavisService.status === 200) {
+			requestDecrypted = await useServiceDavis(
+				request.llavePrivada,
+				JSON.parse(request.texto)
+			);
+
+			responseDecrypted = await useServiceDavis(
+				response.llavePrivada,
+				JSON.parse(response.texto)
+			);
+			console.log('Se usó el serivicio de Davis');
+		} else {
+			return vscode.window.showErrorMessage(
+				'El servicio de David no esta disponible, favor de pedirle que levante su Docker',
+				'Realizado'
+			);
+			/*requestDecrypted = await desencriptarObjeto(
+				JSON.parse(JSON.stringify(request.texto)),
+				request.llavePrivada
+			);
+			responseDecrypted = await desencriptarObjeto(
+				JSON.parse(JSON.stringify(response.texto)),
+				response.llavePrivada
+			);
+
+			requestDecrypted = Object.keys(requestDecrypted)
+				.map((key: any) => requestDecrypted[key])
+				.join('');
+			responseDecrypted = Object.keys(responseDecrypted)
+				.map((key: any) => responseDecrypted[key])
+				.join('');
+			console.log('Se uso la función interna');*/
+		}
+
+		let textoFinal: string = `
 /***************************************************************
 *               idAcceso: ${idAcceso}
 ****************************************************************
@@ -119,11 +157,11 @@ DESENCRIPTAR RESPONSE (Llave Privada):
 ***************************************************************/
 
 			{
-				"REQUEST": ${JSON.stringify(requestDecrypted)},
-				"REPSONSE": ${JSON.stringify(responseDecrypted)}
+				"**** REQUEST ****": ${JSON.stringify(requestDecrypted)},
+				"**** REPSONSE ****": ${JSON.stringify(responseDecrypted)}
 			}`;
 
-		const writeData = Buffer.from(textoFinal, 'utf8');
+		const writeData = Buffer.from(textoFinal, 'utf8'); // 202211141149554286403
 		const folderUri = pathToSave;
 		const fileUri = folderUri.with({
 			path: posix.join(
@@ -134,11 +172,9 @@ DESENCRIPTAR RESPONSE (Llave Privada):
 		vscode.workspace.fs.writeFile(fileUri, writeData);
 
 		vscode.window.showInformationMessage(
-			`Archivo guardado en:\n${fileUri.path}`,
+			`Fin de la tarea de desencriptado.\nArchivo guardado en:\n${fileUri.path}`,
 			'Entendido'
 		);
-
-		vscode.window.showInformationMessage(`Fin de la tarea de desencriptado`);
 	};
 
 	context.subscriptions.push(disposable);
